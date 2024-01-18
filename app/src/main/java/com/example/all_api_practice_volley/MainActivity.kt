f3d13a60eef36b675334e8545db4ea184e896d02
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-01-18 09:33:40 +0530

    this is my first commit

---
+++ app/src/main/java/com/example/all_api_practice_volley/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.all_api_practice_volley
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
